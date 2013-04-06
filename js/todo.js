f873a41923bc1e566a127660d2d6cb112e81835b
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-04-06 16:54:11 +0200

    Define model in js

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,19 @@
-console.log('test');
+(function ($) {
+  $(function () {
+    'use strict';
+
+    var app = {};
+
+    // Our basic **Todo** model has `pid`, `uid`, and `text` attributes.
+    app.Todo = Backbone.Model.extend({
+      // Default attributes for the todo
+      defaults: {
+        pid: '',
+      uid: 0,
+      text: ''
+      }
+    });
+
+    console.log('test-2');
+  });
+})(jQuery);
